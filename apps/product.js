29b13a992225ecc8a8b899cc0e12a9141b5818e1
Vinhdtgcs200644 <vinhdtgcs200644@fpt.edu.vn>
2021-11-09 11:10:32 -0800

    new updates

--- apps/product.js
+++ apps/product.js
@@ -1,30 +1,94 @@
 const schema = require("../models/products");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
+// function StringID(length) {
+//      var result           = '';
+//      var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//      var charactersLength = characters.length;
+//      for ( var i = 0; i < length; i++ ) {
+//           result += characters.charAt(Math.floor(Math.random() *
+//           charactersLength));
+//      }
+//      return result;
+// }
+
+
+const fileStorageEngine = multer.diskStorage({
+     destination:(req,file,cb)=>{
+          cb(null,"./images")
+     },
+     filename:(req,file,cb)=>{
+          cb(null, Date.now() + "--" + file.originalname);
+     }
+});
+
+const upload = multer({storage:fileStorageEngine});
 
 function ProductPage(req,res){
 
-     var data;
+     schema.find((err, data)=>{
 
-     schema.find()
-          .then((result)=>{
-               console.log("Successfully found");
-               console.log("Data :");
-               console.log(result);
-          }).catch((err)=>{
-               console.log("Error : " + err);
-          });
-     res.render("product", {
-          myVar : "hello there"
+          var printdata = [[]];
+          var count=0;
+          var index=0;
+
+          if(err){
+               console.log("Error");
+               console.log(err);
+               res.send("Error");
+               return;
+          }
+
+          for(var i in data){
+               printdata[index].push([
+                    data[i].title,
+                    data[i].price,
+                    data[i].description,
+                    data[i].imglink
+               ]);
+
+               count+=1;
+               if(count==4){
+                    printdata.push([]);
+                    count=0;
+                    index+=1;
+               }
+          }
+
+          res.render("product",{data:printdata});
      });
+
 }
 
 function ProductManagePage(req, res){
-
      res.render("prodview", {
                checkvar:1,
                data:["0","0","0","0"]
           }
      );
+}
+
+
+function AddProduct(req, res){
+     if(!req.body){
+          return res.sendStatus(400);
+     }
+
+     var title = req.body.title;
+     var price = req.body.price;
+     var origin = req.body.origin;
+     var descript = req.body.description;
+     if(descript==""){
+          descript="None";
+     }
+
+     if(title=="" ||price=="" ||origin=="None"||req.file==undefined){
+          res.redirect("/prodview");
+     }
+
+     return;
 }
 
 function UpdateProd(req, res){
@@ -67,8 +131,48 @@ function UpdateProd(req, res){
      });
 }
 
+function Search(req, res){
+     res.render("search",{result:[]});
+}
+
+function SearchProcess(req,res){
+
+     if(!req.body || req.body.search==""){
+          res.redirect("search");
+          return;
+     }
+
+     schema.find({title:req.body.search}, (error, data)=>{
+          if(error){
+               res.send("something went wrong");
+               console.log(error);
+               return;
+          }
+
+          if(data.length==0){
+               res.render("search",{
+                    result:[null]
+               });
+               return;
+          }
+          res.render("search", {
+               result:[
+                    data[0].title,
+                    data[0].price,
+                    data[0].origin,
+                    data[0].description,
+                    data[0].imglink
+               ]
+          });
+     });
+}
+
 module.exports = {
      _ProdManage_ : ProductManagePage,
      _ProdList_ : ProductPage,
      _Product_Update : UpdateProd,
+     _AddProduct_ : AddProduct,
+     _SearchProd_ : Search,
+     _SearchAnalyze_ : SearchProcess,
+     upload:upload
 };
--- apps/report.js
+++ apps/report.js
@@ -1,5 +1,12 @@
-function ReportPage(req,res){ // Bill totals
+const schema = require("../models/bills");
 
+
+function ReportPage(req,res){ // Bill totals
+     schema.find((err,data)=>{
+          res.render("report", {
+               data:data
+          });
+     });
 }
 
 module.exports = {
